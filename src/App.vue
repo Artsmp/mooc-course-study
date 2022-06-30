a70be896043faba108fd6fa95b9730754dfc7454
artsmp <603703819@qq.com>
2022-06-30 12:42:39 +0800

    feat: 项目结构初始化

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,19 @@
 
 <template>
   <div>
-    <span class="text-lg bg-pink-400">123</span>
+    <span
+      class="text-lg bg-pink-400 inline-block m-2 p-3 rounded-md shadow-2xl shadow-pink-400 text-center"
+      >123</span
+    >
+    <button class="btn btn-green">TEST</button>
   </div>
 </template>
+
+<style>
+.btn {
+  @apply py-2 px-4 rounded-sm shadow-sm;
+}
+.btn-green {
+  @apply bg-green-600 text-white transition shadow-green-600 hover:bg-green-700 hover:shadow-green-700;
+}
+</style>
